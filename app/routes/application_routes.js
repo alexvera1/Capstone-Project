b8404f1435532315c6835712d0ecb2333465636b
alexvera1 <sfa.vera@gmail.com>
2022-03-05 17:37:45 -0800

    commit backend api models and routes

--- app/routes/application_routes.js
+++ app/routes/application_routes.js
@@ -3,8 +3,8 @@ const express = require('express')
 // Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
 
-// pull in Mongoose model for examples
-const Example = require('../models/example')
+// pull in Mongoose model for applications
+const Application = require('../models/application')
 
 // this is a collection of methods that help us detect situations when we need
 // to throw a custom error
@@ -17,7 +17,7 @@ const handle404 = customErrors.handle404
 const requireOwnership = customErrors.requireOwnership
 
 // this is middleware that will remove blank fields from `req.body`, e.g.
-// { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
+// { Application: { title: '', text: 'foo' } } -> { Application: { text: 'foo' } }
 const removeBlanks = require('../../lib/remove_blank_fields')
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
@@ -28,37 +28,37 @@ const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
 // INDEX
-// GET /examples
-router.get('/examples', requireToken, (req, res, next) => {
-  Example.find()
-    // respond with status 200 and JSON of the examples
-    .then(examples => res.status(200).json({ examples: examples }))
+// GET /applications
+router.get('/applications', requireToken, (req, res, next) => {
+  Application.find()
+    // respond with status 200 and JSON of the applications
+    .then(applications => res.status(200).json({ applications: applications }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // SHOW
-// GET /examples/5a7db6c74d55bc51bdf39793
-router.get('/examples/:id', requireToken, (req, res, next) => {
+// GET /applications/5a7db6c74d55bc51bdf39793
+router.get('/applications/:id', requireToken, (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
-  Example.findById(req.params.id)
+  Application.findById(req.params.id)
     .then(handle404)
-    // if `findById` is succesful, respond with 200 and "example" JSON
-    .then(example => res.status(200).json({ example: example }))
+    // if `findById` is succesful, respond with 200 and "Application" JSON
+    .then(application => res.status(200).json({ application: application }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // CREATE
-// POST /examples
-router.post('/examples', requireToken, (req, res, next) => {
-  // set owner of new example to be current user
-  req.body.example.owner = req.user.id
+// POST /applications
+router.post('/applications', requireToken, (req, res, next) => {
+  // set owner of new Application to be current user
+  req.body.application.owner = req.user.id
 
-  Example.create(req.body.example)
-    // respond to succesful `create` with status 201 and JSON of new "example"
-    .then(example => {
-      res.status(201).json({ example })
+  Application.create(req.body.application)
+    // respond to succesful `create` with status 201 and JSON of new "Application"
+    .then(application => {
+      res.status(201).json({ application })
     })
     // if an error occurs, pass it off to our error handler
     // the error handler needs the error message and the `res` object so that it
@@ -67,18 +67,18 @@ router.post('/examples', requireToken, (req, res, next) => {
 })
 
 // UPDATE
-// PATCH /examples/5a7db6c74d55bc51bdf39793
-router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
+// PATCH /applications/5a7db6c74d55bc51bdf39793
+router.patch('/applications/:id', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
-  delete req.body.example.owner
+  delete req.body.application.owner
 
-  Example.findById(req.params.id)
+  Application.findById(req.params.id)
     .then(handle404)
-    // ensure the signed in user (req.user.id) is the same as the example's owner (example.owner)
-    .then(example => requireOwnership(req, example))
-    // updating example object with exampleData
-    .then(example => example.updateOne(req.body.example))
+    // ensure the signed in user (req.user.id) is the same as the Application's owner (Application.owner)
+    .then(application => requireOwnership(req, application))
+    // updating Application object with ApplicationData
+    .then(application => Application.updateOne(req.body.application))
     // if that succeeded, return 204 and no JSON
     .then(() => res.sendStatus(204))
     // if an error occurs, pass it to the handler
@@ -86,14 +86,14 @@ router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
 })
 
 // DESTROY
-// DELETE /examples/5a7db6c74d55bc51bdf39793
-router.delete('/examples/:id', requireToken, (req, res, next) => {
-  Example.findById(req.params.id)
+// DELETE /applications/5a7db6c74d55bc51bdf39793
+router.delete('/applications/:id', requireToken, (req, res, next) => {
+  Application.findById(req.params.id)
     .then(handle404)
-     // ensure the signed in user (req.user.id) is the same as the example's owner (example.owner)
-    .then(example => requireOwnership(req, example))
-    // delete example from mongodb
-    .then(example => example.deleteOne())
+  // ensure the signed in user (req.user.id) is the same as the Application's owner (Application.owner)
+    .then(application => requireOwnership(req, application))
+    // delete Application from mongodb
+    .then(application => Application.deleteOne())
     // send back 204 and no content if the deletion succeeded
     .then(() => res.sendStatus(204))
     // if an error occurs, pass it to the handler
--- curl-scripts/applications/update.sh
+++ curl-scripts/applications/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/applications"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -9,8 +9,11 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
 --header "Authorization: Bearer ${TOKEN}" \
 --data '{
-    "example": {
-      "text": "'"${TEXT}"'"
+    "application": {
+      "company": "'"${COMPANY}"'",
+      "role": "'"${ROLE}"'",
+      "salary": "'"${SALARY}"'",
+      "status": "'"${STATUS}"'"
     }
   }'
 
